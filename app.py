aff8a89ad61f1cb2d7f8b41e5d87847299ea200a
Ollie Grooby <olliegrooby@gmail.com>
2018-10-03 15:11:48 +0100

    Added log in, and basic ping

--- app.py
+++ app.py
@@ -9,3 +9,20 @@ try:
 		pprint(config)
 except: 
 	print("There was an error reading from the config file, please ensure you have one!")
+
+token = config["token"]
+prefix = config["prefix"]
+
+bot = discord.Client()
+
+@bot.event
+async def on_ready():
+	print("Logged in as ", bot.user.name)
+
+@bot.event
+async def on_message(message):
+	if message.content.startswith(prefix):
+		if "ping" in message.content:
+			pong = await bot.send_message(message.channel, "Pong!")
+
+bot.run(token)
